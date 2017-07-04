b5acd48b110c8c29424439516c7404f684a82c35
Zap0xfce2 <zap0xfce2@chaos-lordz.net>
2017-07-04 20:20:54 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,15 @@
-# Treepers (for Sponge)
-A Sponge plugin for Minecraft. Stops Creepers from destroying blocks and plants a tree instead.
+# Colorhelper (for Sponge)
+A Sponge plugin for Minecraft. Helps you with the color codes.
 
-**Current Version API 5.0.0:** [5.1.3 M0d-V1 (download)](https://raw.githubusercontent.com/zap0xfce2/treepers-sponge/master/release/Treepers-5.1.3-ZPX-M0d-V1.jar)
+**Current Version API 6.0.0:** [6.1.0 (download)](https://raw.githubusercontent.com/zap0xfce2/colorhelper-sponge/master/release/Colorhelper-6.1.0.jar)
 
 ## Description
-Tired of cleaning up the result of a Creeper meeting an unalert player? Want to save your precioussSSsssSssSS things? But still want to make encountering these creatures remarkable?
+Displays the colorcodes if you type /colorhelper in the chat.
 
-**Treepers** not only stops the destructive behaviour of a Creeper's explosion, but grows a **tree** to remember him!
-
-![Treeper animation](https://raw.githubusercontent.com/zap0xfce2/treepers-sponge/master/anim.gif)
-
-The type of tree grown is determined by the current biome.
+![Colorhelper](https://raw.githubusercontent.com/zap0xfce2/colorhelper-sponge/master/screenshot.png)
 
 ## Changelog
-* **5.1.3 M0d-V1**: Added TREEPER_CHANCE to the config so that creepers and treepers can appear
-* **5.1.3**: Fixed plugin API and methods to meet requirements
-* **5.1.2**: API 5 Version (test build)
-* **1.2**: Included config
-* **1.1**: Fixed biome fetching and y-Position check
-* **1.0**: Initial release.
+* **6.1.0**: Initial release.
 
 ## Infos for Devs
-* develop branch is the latest for API 5
+* develop branch is the latest for API 6
---
+++ src/main/java/org/zpx/sponge/colorhelper/Colorhelper.java
@@ -0,0 +1,52 @@
+package org.zpx.sponge.colorhelper;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.serializer.TextSerializers;
+import com.google.inject.Inject;
+import org.spongepowered.api.plugin.PluginContainer;
+
+
+@Plugin(id = "clolorhelper", name = "Colorhelper", version = "6.1.0", authors = "Zap0xfce2",
+        url = "https://github.com/zap0xfce2/colorhelper-sponge",
+        description = "Helps you with the color codes.")
+public class Colorhelper {
+
+    @Inject
+    private Logger logger;
+    @Inject
+    private PluginContainer container;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent ServerStartEvent) {
+
+        CommandSpec base = CommandSpec.builder()
+                .permission("colorhelper.use")
+                .description(Text.of("Shows the Colorhelper."))
+                .executor((src, args) -> {
+                    src.sendMessage(toColoredText("&b--------------------- Colorhelper ---------------------"));
+                    src.sendMessage(toColoredText("&4Darkred (4)&f, &cRed (c)&f, &6Gold (6)&f, &eYellow (e)&f, &2Dark Green (2)&f, &aGreen (a)&f, &bAqua (b)&f, &3Dark Aqua (3)&f, &1Dark Blue (1)&f, &9Blue (9)&f, &dLight Purple (d)&f, &5Dark Purple (5)&f, &fWhite (f)&f, &7Gray (7)&f, &8Dark Gray (8)&f, &0Black (0)&f, "));
+                    src.sendMessage(toColoredText("&lBold (l)&r, &nUnderline (n)&r, &oItallic (o)&r, &kMagic &f(k)&r, &mStrike (m)&r, &rReset (r)"));
+                    src.sendMessage(toColoredText("&b--------------------- Colorhelper ---------------------"));
+                    return CommandResult.success();
+                }).build();
+        Sponge.getCommandManager().register(this, base, "colorhelper");
+
+        // Log Start Up to Console
+        logger.info(
+                container.getName() + " version " + container.getVersion().orElse("")
+                        + " enabled!");
+
+    }
+
+    public static Text toColoredText(String str){
+        return TextSerializers.FORMATTING_CODE.deserialize(str);
+    }
+
+}
